0edd8f868121a63e92d1d35e6024aa413626d218
andres alvarez <75365680+andresalvareez@users.noreply.github.com>
2023-01-24 18:03:58 +0100

    copiar direccion terminado

---
+++ cpd.zsh
@@ -0,0 +1,19 @@
+direccion=$(pwd)
+
+# Función para cambiar los espacios por "\ "
+function cambiar_espacios {
+    direccion=${direccion// /\\ }
+}
+
+# Verificando si hay espacios en blanco en la dirección
+if [[ $direccion =~ " " ]]; then
+    # Llamando a la función para cambiar los espacios
+    cambiar_espacios
+    echo "$direccion"
+else
+    # Recorriendo la dirección letra por letra
+    for (( i=0; i<${#direccion}; i++ )); do
+    # Imprimiendo cada letra
+        echo ${direccion:$i:1}
+    done
+fi
